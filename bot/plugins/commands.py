b0202978f458675a43c677780020e492ec91c7a4
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-08-29 13:38:52 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -139,7 +139,7 @@ Join on our channel to get movies ✅
     await bot.send_message(
 
         chat_id=update.chat.id,
-        photo = 'https://telegra.ph/file/6824a00f2babc136862cd.jpg',
+        
         text=Translation.START_TEXT.format(
 
                 update.from_user.first_name),
